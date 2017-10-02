f235fa2bb5c279c5d8c921a2889f5e0cc9a6141a
rramos20 <rramos20@mail.csuchico.edu>
2017-10-02 16:58:37 -0700

    put in user comments

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "bitmap.h"
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+Bitmap image;
+vector <vector <Pixel> > bmp;
+
+
+//ask the user for an images file name
+
+
+//read a file example.bmp and convert it to a pixel matrix
+
+
+//save the image and display the image back to the user in grayscale
+
+
+
+//if the program doesn't work tell the user why and have them re-enter the file name again
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0
+}
