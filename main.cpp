d514edf7a5207cb33a09ab48724f3c26562c00ad
rramos20 <rramos20@mail.csuchico.edu>
2017-10-13 14:22:27 -0700

    need to finish fixing the while loop

--- main.cpp
+++ main.cpp
@@ -10,21 +10,23 @@ Bitmap image;
 vector <vector <Pixel> > bmp;
 Pixel rgb;
 string filename;
-
 //ask the user for an images file name
-cout << "Wanna see a cool picture? Type machupicchu.bmp" << endl;
+cout << "What file do you want converted?" << endl;
 cin >> filename;   
         
 //read a file example.bmp and convert it to a pixel matrix
-
 image.open(filename);
-
 bool validBMP = image.isImage();
-while(validBMP == true)
+while(validBMP != true)
 {
-
+cout<<"It appears something went wrong, please re-enter the file name and make sure it is a 24 bit depth Windows BMP file."<<endl;
+cout<<"What file do you want to convert?"<<endl;
+cin>>filename;
+image.open(filename);
+bool validBMP = image.isImage();
+}
 bmp = image.toPixelMatrix();
-cout << "machupicchu.bmp has been loaded. It is " <<bmp.size()<<" pixelswide and "<<bmp[0].size()<<" pixels high."<<endl;
+cout <<filename<< "  has been loaded. It is " <<bmp.size()<<" pixelswide and "<<bmp[0].size()<<" pixels high."<<endl;
 
 //save the image and display the image back to the user in grayscale
     for(int i=0; i<bmp.size();i++)
@@ -43,12 +45,9 @@ cout << "machupicchu.bmp has been loaded. It is " <<bmp.size()<<" pixelswide and
             }    
         }
 image.fromPixelMatrix(bmp);
-image.save("MachuPicchux2");
+image.save("OldTimey.bmp");
+    
 //if the program doesn't work tell the user why and have them re-enter the file name again
-while(validBMP == false)
-    {
-cout<<"It appears something went wrong, please re-enter the file name."<<endl;
-    }
-}
+
 return 0;
 }
